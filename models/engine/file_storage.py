09d7880975a26f2a53e1bc9055a87b3840cf13b2
root <root@MSI>
2023-10-31 18:02:58 -0500

    reverting filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,67 +1,70 @@
 #!/usr/bin/python3
-"""Manages file storage"""
 import json
-from models.amenity import Amenity
+import os
+from typing import Dict, Type
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
 from models.city import City
+from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
-
 
 class FileStorage:
-    """serializes to json"""
-    __file_path = 'file.json'
-    __objects = {}
+    def __init__(self):
+        self.file_path = "./file.json"
+        self.objects = {}
 
-    def all(self, cls=None):
-        """returns the dictionary __objects"""
+    def clear(self):
+        self.objects = {}
+
+    def all(self, cls=None) -> Dict[str, object]:
         if cls is not None:
-            new_dict = {}
-            for key, value in self.__objects.items():
-                if cls == value.__class__ or cls == value.__class__.__name__:
-                    new_dict[key] = value
-            return new_dict
-        return self.__objects
+            return {key: obj for key, obj in self.objects.items() if isinstance(obj, cls)}
+        return self.objects
 
-    def new(self, obj):
-        """adds to storage"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+    def new(self, obj: object):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.objects[key] = obj
 
     def save(self):
-        """saves dict to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        nd = {key: value.to_dict() for key, value in self.objects.items()}
+        with open(self.file_path, "w") as f:
+            json.dump(nd, f)
 
     def reload(self):
-        """loads dict from file"""
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+            with open(self.file_path, "r", encoding="UTF-8") as f:
+                reloaded = json.load(f)
+                for obj_id, obj_data in reloaded.items():
+                    class_name = obj_data.get("__class__")
+                    cls_func = self.get_class(class_name)
+                    if cls_func:
+                        obj = cls_func(**obj_data)
+                        self.objects[obj_id] = obj
         except FileNotFoundError:
+            # Handle the case when the file doesn't exist
             pass
+        except Exception as e:
+            # Handle other exceptions, such as JSON decoding errors
+            print(f"Error while reloading data: {e")
 
     def delete(self, obj=None):
-        """deletes obj"""
         if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
-            if key in self.__objects:
-                del self.__objects[key]
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            if key in self.objects:
+                del self.objects[key]
 
-    def close(self):
-        """
-        """
-        self.reload()
+    @staticmethod
+    def get_class(class_name: str) -> Type[object] or None:
+        # Define mappings from class names to class objects
+        class_mappings = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review,
+        }
+        return class_mappings.get(class_name, None)
