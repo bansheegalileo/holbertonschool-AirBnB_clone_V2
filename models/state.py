8da9f16502d62056e5a00d767d99296fcc758ea1
root <root@MSI>
2023-11-20 15:25:07 -0600

    taking the l on that one

--- models/state.py
+++ models/state.py
@@ -9,29 +9,21 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-
 class State(BaseModel, Base):
-    """Representation of State"""
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+        cities = []
 
-    def __init__(self, *args, **kwargs):
-        """
-        init inherited
-        """
-        super().__init__(*args, **kwargs)
-
-    if models.storage_type != "db":
-        @property
-        def cities(self):
-            """getter for cities that return
-            a list of city instance equale to
-            curent state id
-            """
-            list_city = []
-            all_inst_c = models.storage.all(City)
-            for value in all_inst_c.values():
-                if value.state_id == self.id:
-                    list_city.append(value)
-            return list_city
+    def cities(self):
+        """city"""
+        city_list = []
+        for city_id in self.cities:
+            city = models.storage.get(City, city_id)
+            if city:
+                city_list.append(city)
+        return city_list
