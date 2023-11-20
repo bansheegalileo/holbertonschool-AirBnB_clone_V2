b75066a1e950b40b5fad66d013fe3cd5e69429ef
root <root@MSI>
2023-11-20 15:24:15 -0600

    reworking state

--- models/state.py
+++ models/state.py
@@ -9,21 +9,29 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
-        cities = []
 
-    def cities(self):
-        """city getter"""
-        city_list = []
-        for city_id in self.cities:
-            city = models.storage.get(City, city_id)
-            if city:
-                city_list.append(city)
-        return city_list
+class State(BaseModel, Base):
+    """Representation of State"""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    def __init__(self, *args, **kwargs):
+        """
+        init inherited
+        """
+        super().__init__(*args, **kwargs)
+
+    if models.storage_type != "db":
+        @property
+        def cities(self):
+            """getter for cities that return
+            a list of city instance equale to
+            curent state id
+            """
+            list_city = []
+            all_inst_c = models.storage.all(City)
+            for value in all_inst_c.values():
+                if value.state_id == self.id:
+                    list_city.append(value)
+            return list_city
